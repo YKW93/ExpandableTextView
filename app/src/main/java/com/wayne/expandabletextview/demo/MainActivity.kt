654379bb5e1bed3ed208735cbb0523aa5a670216
YKW93 <rhkddn1657@gmail.com>
2020-08-27 17:16:26 +0900

    Added Sample code

--- app/src/main/java/com/wayne/expandabletextview/demo/MainActivity.kt
+++ app/src/main/java/com/wayne/expandabletextview/demo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.wayne.expandabletextview.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wayne.expandabletextview.ExpandableTextView
+import com.wayne.expandabletextview.OnStateChangeListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        et_test.stateChangeListener = object : OnStateChangeListener {
+            override fun onStateChanged(state: ExpandableTextView.State) {
+                tv_current_state.text = state.name
+            }
+        }
     }
 }
