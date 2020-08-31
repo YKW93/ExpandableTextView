f236e10f06029f13c7581a7db0e246b5a219df3f
YKW93 <rhkddn1657@gmail.com>
2020-08-31 13:31:04 +0900

    Update sample project  - change font

--- app/src/main/java/com/wayne/expandabletextview/demo/MainActivity.kt
+++ app/src/main/java/com/wayne/expandabletextview/demo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.wayne.expandabletextview.demo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import com.wayne.expandabletextview.ExpandableTextView
 import com.wayne.expandabletextview.OnStateChangeListener
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,5 +18,7 @@ class MainActivity : AppCompatActivity() {
                 tv_current_state.text = state.name
             }
         }
+        // change font
+        et_test.textTypeface = ResourcesCompat.getFont(this, R.font.roboto_bold)
     }
 }
